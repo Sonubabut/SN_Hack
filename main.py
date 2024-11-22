c0bc8176f114a0d822eda04b62e0bae868401b93
Sonu Babu T <111742766+Sonubabut@users.noreply.github.com>
2024-11-23 02:07:50 +0530

    Added multi-cloud apps

--- main.py
+++ main.py
@@ -354,10 +354,10 @@ def main():
         # Application links
         st.markdown("### Applications Under Observability")
         st.markdown(
-            """
-           1. [Meddy - SambaNova Cloud - Azure](https://meddy.azurewebsites.net/)
-           2. [Legal Expert - SambaNova Cloud - AWS](https://master.ddk2t95xjqic2.amplifyapp.com/)
-           3. [Flash AI - SambaNova Cloud - GCP](https://flashai-713269396353.us-central1.run.app/)
+           """
+            - [Meddy - SambaNova Cloud - Azure](https://meddy.azurewebsites.net/)
+            - [Legal Expert - SambaNova Cloud - AWS](https://master.ddk2t95xjqic2.amplifyapp.com/)
+            - [Flash AI - SambaNova Cloud - GCP](https://flashai-713269396353.us-central1.run.app/)
             """,
             unsafe_allow_html=True
         )
